43ca0dff9164c2ad99d790d6100a2782b37d8344
Shangzhen Yang <20525492+shangzhenyang@users.noreply.github.com>
2019-04-29 11:01:26 -0400

    1.2

--- js/index.js
+++ js/index.js
@@ -9,10 +9,7 @@ fetch("https://raw.githubusercontent.com/shangzhenyang/electron-hot-updater/mast
 	}
 }).then(data=>{
 	if(data&&package.version!=data.version){
-		const update=confirm("There are updates available. Do you want to update this app?")
-		if(update){
-			location.href="update.html"
-		}
+		location.href="update.html"
 	}
 	/* If the version number on the server does not match the local version number, open the update page.
 	如果服务器上的版本号与本地的不匹配，打开更新页面。 */
